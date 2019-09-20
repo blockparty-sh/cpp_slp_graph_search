16ebe7a6b51e9f63202879082a1757ac2be90144
blockparty <hello@blockparty.sh>
2019-09-19 19:19:20 -0500

    add catch2 for tests

---
+++ test/010-TestCase.cpp
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
+#include "txgraph.hpp"
+
+
+int create_txgraph()
+{
+    txgraph g;
+    return 1;
+}
+
+TEST_CASE( "create_txgraph is 1 (pass)", "[single-file]" ) {
+    REQUIRE( create_txgraph() == 1 );
+}
