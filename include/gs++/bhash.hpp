3c01b8311b5a4c72a52053692f5165e584af08fc
blockparty <hello@blockparty.sh>
2019-09-21 03:47:44 -0500

    minor fixes

--- include/gs++/bhash.hpp
+++ include/gs++/bhash.hpp
@@ -13,8 +13,6 @@ namespace gs {
 template <typename Tag, unsigned Size = 32>
 struct bhash
 {
-    using underlying_type = Tag;
-
     std::array<std::uint8_t, Size> v;
 
     bhash()
@@ -28,6 +26,9 @@ struct bhash
         for (unsigned i=0; i<Size; ++i) {
             const char p1 = v_[(i<<1)+0];
             const char p2 = v_[(i<<1)+1];
+
+            assert((p1 >= '0' && p1 <= '9') || (p1 >= 'a' && p1 <= 'f'));
+            assert((p2 >= '0' && p2 <= '9') || (p2 >= 'a' && p2 <= 'f'));
 
             v[i] = ((p1 >= '0' && p1 <= '9' ? p1 - '0' : p1 - 'a' + 10) << 4)
                  +  (p2 >= '0' && p2 <= '9' ? p2 - '0' : p2 - 'a' + 10);
@@ -46,14 +47,14 @@ struct bhash
     auto end()   -> decltype(v.end())
     { return v.end(); }
 
-    bool operator==(const bhash<Tag> &o) const
+    bool operator==(const bhash<Tag, Size> &o) const
     { return v == o.v; }
 
-    bool operator!=(const bhash<Tag> &o) const
+    bool operator!=(const bhash<Tag, Size> &o) const
     { return ! operator==(o); }
 
     template <typename H>
-    friend H AbslHashValue(H h, const bhash<Tag>& m)
+    friend H AbslHashValue(H h, const bhash<Tag, Size>& m)
     {
         return H::combine(std::move(h), m.v);
     }
@@ -67,8 +68,8 @@ struct bhash
 
         std::string ret(Size*2, '\0');
         for (unsigned i=0; i<Size; ++i) {
-            ret[(i<<1)+0] = (chars[static_cast<std::uint8_t>(v[i]) >> 4]);
-            ret[(i<<1)+1] = (chars[static_cast<std::uint8_t>(v[i]) & 0x0F]);
+            ret[(i<<1)+0] = chars[v[i] >> 4];
+            ret[(i<<1)+1] = chars[v[i] & 0x0F];
         }
 
         return ret;
