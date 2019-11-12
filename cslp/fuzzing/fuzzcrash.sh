13bbba6604830665eca868336159bededb98fc59
blockparty <hello@blockparty.sh>
2019-11-12 01:46:10 -0600

    add bigger memory+ram limit for now

--- cslp/fuzzing/fuzzcrash.sh
+++ cslp/fuzzing/fuzzcrash.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-afl-fuzz -i corpus -x slp.dict -o out/fuzzcrash ./../../build-afl/cslp/fuzzing/cslp_fuzzcrash @@
+afl-fuzz -i corpus -x slp.dict -o out/fuzzcrash -m1000 -t1000 ./../../build-afl/cslp/fuzzing/cslp_fuzzcrash @@
