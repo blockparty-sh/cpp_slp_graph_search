b463d80d8224d3a6dcbabac1c5c66abdbf1047d9
blockparty <hello@blockparty.sh>
2019-11-24 16:38:11 -0600

    reverse proxy wip

---
+++ server/proxy/reverseproxy.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	gw "./pb"
+)
+
+var (
+	echoEndpoint = flag.String("echo_endpoint", "localhost:9090", "endpoint of YourService")
+)
+
+func run() error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	conn, err := dial(ctx, opts.GRPCServer.Network, opts.GRPCServer.Addr)
+	if err != nil {
+		return err
+	}
+
+	mux := runtime.NewServeMux()
+	gw, err := gw.RegisterGraphSearchServiceServer(ctx, mux, conn)
+	if err != nil {
+		return err
+	}
+
+	return http.ListenAndServe(":8080", mux)
+}
+
+func main() {
+	flag.Parse()
+	defer glog.Flush()
+
+	if err := run(); err != nil {
+		glog.Fatal(err)
+	}
+}
